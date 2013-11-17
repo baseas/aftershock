89662a5ab2cc9e2a2a660b94987fc38fa66f58bc
baseas <baseas@users.noreply.github.com>
2013-11-17 18:58:44 +0100

    Add local makefile and game name

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -27,15 +27,15 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // A user mod should never modify this file
 
 #ifdef STANDALONE
-  #define PRODUCT_NAME			"iofoo3"
-  #define BASEGAME			"foobar"
-  #define CLIENT_WINDOW_TITLE     	"changeme"
-  #define CLIENT_WINDOW_MIN_TITLE 	"changeme2"
-  #define HOMEPATH_NAME_UNIX		".foo"
-  #define HOMEPATH_NAME_WIN		"FooBar"
+  #define PRODUCT_NAME			"aftershock"
+  #define BASEGAME			"data"
+  #define CLIENT_WINDOW_TITLE     	"Aftershock"
+  #define CLIENT_WINDOW_MIN_TITLE 	"Aftershock"
+  #define HOMEPATH_NAME_UNIX		".aftershock"
+  #define HOMEPATH_NAME_WIN		"Aftershock"
   #define HOMEPATH_NAME_MACOSX		HOMEPATH_NAME_WIN
-  #define GAMENAME_FOR_MASTER		"foobar"	// must NOT contain whitespace
-//  #define LEGACY_PROTOCOL	// You probably don't need this for your standalone game
+  #define GAMENAME_FOR_MASTER		"aftershock"	// must NOT contain whitespace
+  #define LEGACY_PROTOCOL	// You probably don't need this for your standalone game
 #else
   #define PRODUCT_NAME			"ioq3"
   #define BASEGAME			"baseq3"
